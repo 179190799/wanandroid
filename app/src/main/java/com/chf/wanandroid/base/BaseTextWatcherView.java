c86a0acd0b2212830b586ef4a67c8f41a82a6637
THINK <179190799@qq.com>
2018-04-11 17:55:20 +0800

    新增一个基类

---
+++ app/src/main/java/com/chf/wanandroid/base/BaseTextWatcherView.java
@@ -0,0 +1,11 @@
+package com.chf.wanandroid.base;
+
+import android.text.TextWatcher;
+
+/**
+ * Created by THINK on 2018/4/11.
+ */
+
+public interface BaseTextWatcherView extends BaseView ,TextWatcher {
+    
+}
